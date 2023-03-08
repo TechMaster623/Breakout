337339ce9f1656eda57104738e95ad85e53f25e9
Faiz Ali <fali@bsd220.org>
2023-03-08 17:59:27 -0600

    created ball object

--- Breakout/GameViewController.swift
+++ Breakout/GameViewController.swift
@@ -18,7 +18,7 @@ class GameViewController: UIViewController {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .resizeFill
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -42,4 +42,7 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
-}
+    
+    
+    }
+
