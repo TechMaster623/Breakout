85d5edf635d1ba563008a7c20ec4a04f712b59b5
Faiz Ali <fali@bsd220.org>
2023-03-17 18:03:23 -0500

    set up multiple rows of multicolored bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -166,27 +166,38 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 contact.bodyB.node == brick {
                 score += 1
                 updateLabels()
-                brick.removeFromParent()
-                removedBricks += 1
-                if removedBricks == bricks.count {
-                    gameOver(winner: true)
+                if brick.color == .blue {
+                    brick.color = .orange
                 }
-                
-            }
-            if contact.bodyA.node?.name == "loseZone" ||
-                contact.bodyB.node?.name == "loseZone" {
-                lives -= 1
-                if lives > 0 {
-                    score = 0
-                    resetGame()
-                    kickBall()
-                    
+                else if brick.color == .orange {
+                    brick.color = .green
                 }
                 else {
-                    gameOver(winner: false)
+                    
+                    
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
+                    
                 }
             }
-        }
+                if contact.bodyA.node?.name == "loseZone" ||
+                    contact.bodyB.node?.name == "loseZone" {
+                    lives -= 1
+                    if lives > 0 {
+                        score = 0
+                        resetGame()
+                        kickBall()
+                        
+                    }
+                    else {
+                        gameOver(winner: false)
+                    }
+                }
+            }
+        
         func gameOver(winner: Bool) {
             playingGame = false
             playLabel.alpha = 1
@@ -212,18 +223,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let count = Int(frame.width) / 55
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX)  + 25
-        let y = Int(frame.maxY) - 65
-        for i in 0..<count {
-            let x = i * 55 + xOffset
-            makeBrick(x: x, y: y, color: .green)
+        let colors:  [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3 {
+            let y = Int(frame.maxY) - 65 - (r * 25)
+            for i in 0..<count {
+                let x = i * 55 + xOffset
+                makeBrick(x: x, y: y, color: colors[r])
+            }
         }
+        
     }
     
+    
+    
+    
+    
+    
+    
 }
-        
-        
-        
-        
-
-
-
